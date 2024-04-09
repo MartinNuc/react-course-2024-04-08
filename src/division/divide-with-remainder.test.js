7614f28cafd063e388a2884eaaa54d2e80a9f9a3
MartinNuc <martin.nuc@productboard.com>
2024-04-09 10:59:30 +0200

    wrap in describe

--- src/division/divide-with-remainder.test.js
+++ src/division/divide-with-remainder.test.js
@@ -1,25 +1,26 @@
 import {divideWithRemainder} from './divide-with-remainder';
 
-it('divides 0 by 1 with result 0', () => {
-  const {result} = divideWithRemainder(0, 1);
-  expect(result).toBe(0);
+describe('divideWithRemainder', () => {
+  it('divides 0 by 1 with result 0', () => {
+    const {result} = divideWithRemainder(0, 1);
+    expect(result).toBe(0);
+  });
+  
+  it('divides 5 by 3 with result 1', () => {
+    const {result} = divideWithRemainder(5, 3);
+    expect(result).toBe(1);
+  });
+  
+  it('divides 5 by 3 with remainder 2', () => {
+    const {remainder} = divideWithRemainder(5, 3);
+    expect(remainder).toBe(2);
+  });
+  
+  it('throws an exception when diving by 0', () => {
+    expect(() => divideWithRemainder(5, 0)).toThrow('Cannot divide by zero.');
+  });
+  
+  it('throws an exception when passing non-numeric values', () => {
+    expect(() => divideWithRemainder('ahoj', [])).toThrow('Inputs must be numbers');
+  });
 });
-
-it('divides 5 by 3 with result 1', () => {
-  const {result} = divideWithRemainder(5, 3);
-  expect(result).toBe(1);
-});
-
-it('divides 5 by 3 with remainder 2', () => {
-  const {remainder} = divideWithRemainder(5, 3);
-  expect(remainder).toBe(2);
-});
-
-it('throws an exception when diving by 0', () => {
-  expect(() => divideWithRemainder(5, 0)).toThrow('Cannot divide by zero.');
-});
-
-it('throws an exception when passing non-numeric values', () => {
-  expect(() => divideWithRemainder('ahoj', [])).toThrow('Inputs must be numbers');
-});
-
